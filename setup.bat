02e98d39f71ea88b50c3da3fce2deba17e923953
Tanush Chugh <tanush.chugh@hotmail.com>
2025-04-23 23:31:43 +0530

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Cloning the repository...
+git clone https://github.com/TanushChugh19/Octahedron-AI.git
+cd Octahedron-AI
+
+echo Installing dependencies...
+pip install -r requirements.txt
+
+echo Setup complete! You can now run the bot.
