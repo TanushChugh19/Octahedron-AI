f197417504b0772cdac9f79a61951706d007df49
Tanush Chugh <tanush.chugh@hotmail.com>
2025-04-23 23:46:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright (c) 2025 Tanush Chugh
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # Set repository URL
 REPO_URL="https://github.com/TanushChugh19/Octahedron-AI.git"
 FOLDER_NAME="Octahedron-AI"
