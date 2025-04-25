04d3f16e210be575b633545df77704617f38d75b
Tanush Chugh <tanush.chugh@hotmail.com>
2025-04-26 02:19:47 +0530

    Update app.py

--- apps/llama3.2-vision-11b/app.py
+++ apps/llama3.2-vision-11b/app.py
@@ -29,7 +29,7 @@ if not load_dotenv():
 if __name__ == "__main__":
     load_dotenv()
 
-    ALLOWED_USER = 924697503356583967
+    ALLOWED_USER = 0 #  Put your discord user id here 😊
 
     intents = discord.Intents.default()
     intents.message_content = True
